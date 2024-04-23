d71cea75280aa51b99f08a7a5f8739ce6da64009
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-04-23 22:20:51 +0530

    chnages

--- components/about.tsx
+++ components/about.tsx
@@ -42,7 +42,7 @@ export default function About() {
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the
+        learning how to play the guitar.
       </p>
     </motion.section>
   );
