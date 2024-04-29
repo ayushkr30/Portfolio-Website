9b28d3c19a611cef25c4b16409e7a5627cd27328
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-04-29 21:15:49 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -39,6 +39,9 @@ export default function About() {
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy{" "}
-       
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        </p><span className="font-medium">history and philosophy</span>. I'm also
+        learning how to play the guitar.
      
 }
